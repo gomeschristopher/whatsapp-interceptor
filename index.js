d4983a8788b4c0c600ce4de556db927e1bc9299a
Christopher Gomes <gomeschristopher@hotmail.com>
2022-02-23 17:03:59 -0300

    OA-14

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
-const { Client } = require("whatsapp-web.js");
+const { Client, MessageMedia } = require("whatsapp-web.js");
 const SESSION_FILE_PATH = "./session.json";
 let sessionData;
 const express = require("express");
 const axios = require("axios");
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
+const FormData = require("form-data");
+const multer = require('multer');
+const { isNull } = require("util");
+const upload = multer({ storage: multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'storage/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})});
 
 if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionData = require(SESSION_FILE_PATH);
@@ -32,11 +43,27 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
+  const contact = await msg.getContact();
   try {
-    await axios.post(process.env.API_URL || 'https://api.i5sistemas.com.br/api' + "/conversations/from", {
-      username: msg.from,
-      message: msg.body,
-    });
+    const formData = new FormData();
+    formData.append("username", msg.from);
+    formData.append("body", msg.body);
+    formData.append("pushname", contact.pushname);
+    if (msg.hasMedia) {
+      const media = await msg.downloadMedia();
+      const buff = Buffer.from(media.data, "base64");
+      fs.writeFileSync("storage/" + media.filename, buff);
+      formData.append("file", fs.createReadStream("storage/" + media.filename));
+      formData.append("type", media.mimetype);
+    }
+    await axios.post(
+      (process.env.API_URL || "https://api.i5sistemas.com.br/api") +
+        "/conversations/from",
+      formData,
+      {
+        headers: formData.getHeaders(),
+      }
+    );
   } catch (error) {
     console.error(error);
   }
@@ -47,14 +74,18 @@ client.initialize();
 const app = express();
 app.use(express.json());
 
-app.get('', (req, res) => {
-    res.send('Working!')
+app.get("", (req, res) => {
+  res.send("Working!");
 });
 
-app.post("/send", (req, res) => {
-  const { message, number } = req.body;
-  client.sendMessage(number, message);
-  res.send({ status: "Enviado mensagem!" });
+app.post('/message', upload.single('file'), (req, res) => {
+  let message = req.body.message;
+  if(req.file) {
+    message = MessageMedia.fromFilePath(`./storage/${req.file.filename}`);
+  }
+  res.send(client.sendMessage(req.body.number, message));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server is ready in on port ${process.env.PORT || 3000}`));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server is ready in on port ${process.env.PORT || 3000}`)
+);
