c75b83af7af6f4709c294fe67cdef5f51f143f04
Christopher Gomes <gomeschristopher@hotmail.com>
2022-02-24 07:26:11 -0300

    fix hequest

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const express = require("express");
 const axios = require("axios");
 const qrcode = require("qrcode-terminal");
 const FormData = require("form-data");
-const multer = require('multer');
+const multer = require("multer");
 
-const upload = multer({ storage: multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'storage/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})});
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "storage/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionData = require(SESSION_FILE_PATH);
@@ -45,25 +47,27 @@ client.on("ready", () => {
 client.on("message", async (msg) => {
   const contact = await msg.getContact();
   try {
-    const formData = new FormData();
-    formData.append("username", msg.from);
-    formData.append("body", msg.body);
-    formData.append("pushname", contact.pushname);
+    const url =
+      (process.env.API_URL || "https://api.i5sistemas.com.br/api") +
+      "/messages";
     if (msg.hasMedia) {
       const media = await msg.downloadMedia();
       const buff = Buffer.from(media.data, "base64");
       fs.writeFileSync("storage/" + media.filename, buff);
+      const formData = new FormData();
+      formData.append("username", msg.from);
+      formData.append("body", msg.body);
+      formData.append("pushname", contact.pushname);
       formData.append("file", fs.createReadStream("storage/" + media.filename));
       formData.append("type", media.mimetype);
+      await axios.post(url, formData, formData.getHeaders);
+    } else {
+      await axios.post(url, {
+        username: msg.from,
+        body: msg.body,
+        pushname: contact.pushname,
+      });
     }
-    await axios.post(
-      (process.env.API_URL || "https://api.i5sistemas.com.br/api") +
-        "/messages?method=put",
-      formData,
-      {
-        headers: formData.getHeaders(),
-      }
-    );
   } catch (error) {
     console.error(error);
   }
@@ -78,9 +82,9 @@ app.get("", (req, res) => {
   res.send("Working!");
 });
 
-app.post('/message', upload.single('file'), (req, res) => {
+app.post("/message", upload.single("file"), (req, res) => {
   let message = req.body.message;
-  if(req.file) {
+  if (req.file) {
     message = MessageMedia.fromFilePath(`./storage/${req.file.filename}`);
   }
   res.send(client.sendMessage(req.body.number, message));
