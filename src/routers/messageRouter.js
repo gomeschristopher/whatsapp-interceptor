4f92e1f646b6feecec548dabc9b0584322a62d1d
Christopher <gomeschristopher@hotmail.com>
2023-11-02 07:34:35 -0300

    update packages

--- src/routers/messageRouter.js
+++ src/routers/messageRouter.js
@@ -1,20 +1,7 @@
 
 const express = require('express');
 const router = new express.Router();
-const { MessageMedia } = require("whatsapp-web.js")
 const whatsappclient = require("../services/WhatsappClient")
-const multer = require("multer")
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./storage/")
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    },
-  }),
-})
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
