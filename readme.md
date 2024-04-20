35d84fb11479b9d74d35c063804eb9ebd4d4f5d7
Christopher <gomeschristopher@hotmail.com>
2024-04-20 11:11:34 -0300

    add webversioncache

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # Node.js Rest API to receive and send message across WhatsApp
 
 ## How to use:
+### 1. Run `cp .env.example .env`
 ### 1. Inside the project run `npm install`
 ### 2. Start the app with `npm start`
 ### 3. Authenticate your Whatsapp in terminal with Qr code 
--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -2,7 +2,11 @@ const { Client, LocalAuth } = require("whatsapp-web.js")
 const qrcode = require("qrcode-terminal")
 
 const whatsappclient = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2407.3.html`
+    }
 })
 
 whatsappclient.on("qr", (qr) => qrcode.generate(qr, { small: true }))
